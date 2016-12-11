e4430b90db396e4cb4f2adab02784915b68c9879
Peter Jurkovic <petojurkovic@gmail.com>
2016-12-11 16:57:26 +0000

    first commit

---
+++ src/main/java/io/memit/MemitApiApplication.java
@@ -0,0 +1,12 @@
+package io.memit;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MemitApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MemitApiApplication.class, args);
+	}
+}
---
+++ src/main/java/io/memit/model/Book.java
@@ -0,0 +1,5 @@
+package io.memit.model;
+
+public class Book {
+
+}
